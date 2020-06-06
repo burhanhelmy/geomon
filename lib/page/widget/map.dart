557a49489379f4f84e9edcbef1cdaef96d64e840
Burhan <burhanuddin.j@vettons.com>
2020-06-06 20:26:21 +0800

    added golocation and google map

--- lib/page/widget/map.dart
+++ lib/page/widget/map.dart
@@ -1,15 +1,58 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapWidget extends StatefulWidget {
   @override
-  _MapWidgetState createState() => _MapWidgetState();
+  State<MapWidget> createState() => MapWidgetState();
 }
 
-class _MapWidgetState extends State<MapWidget> {
+class MapWidgetState extends State<MapWidget> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  Set<Circle> circles = Set.from([
+    Circle(
+      fillColor: Colors.blue.withOpacity(0.3),
+      circleId: CircleId('1231dassad'),
+      center: LatLng(37.42796133580664, -122.085749655962),
+      radius: 2000,
+    )
+  ]);
+
+  @override
+  void initState() {
+    _getLocation();
+    super.initState();
+  }
+
+  _getLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+      target: LatLng(position.latitude, position.longitude),
+      zoom: 15,
+    )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green,
+      child: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        circles: circles,
+      ),
     );
   }
 }
